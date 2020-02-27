9140de3f78f0341037358338782d6bd91da21d35
Anisimova <amanisi1@mts.ru>
2020-02-27 23:53:32 +0300

    homework 3

--- docker/run.sh
+++ docker/run.sh
@@ -3,11 +3,11 @@ docker run \
 	--privileged=true \
 	-t -i \
 	--publish-all=true \
-	-p 8888:8888 \
-	-p 9999:9999 \
-	-p 8088:8088 \
-	-p 7180:7180 \
-	-p 80:80 \
+	-p 8889:8888 \
+	-p 9998:9999 \
+	-p 8089:8088 \
+	-p 7181:7180 \
+	-p 8000:80 \
 	-v $(pwd)/..:/workspace \
-	ishugaepov/mlbd \
+	ishugaepov/mlbd_spark2 \
 	/usr/bin/docker-quickstart
